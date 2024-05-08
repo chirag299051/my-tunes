fa5d5b5944561c64d8a3e0861834ae9e9fd0d2c3
Chirag Singh <chirag@Chirags-MacBook-Pro.local>
2024-05-08 15:14:52 +0530

    updates

--- src/Song.js
+++ src/Song.js
@@ -12,7 +12,7 @@ const Song = ({ song }) => {
 
   return (
     <ScrollIntoView className="song" selector=".active">
-      <Link to={`/main/${page}/${id}`}>
+      <Link to={`/my-tunes/main/${page}/${id}`}>
         <span className="span center">{id}.</span>
         <span className="span">
           <i>{title}</i>
@@ -23,7 +23,7 @@ const Song = ({ song }) => {
             onClick={(e) => {
               e.preventDefault();
               e.stopPropagation();
-              navigate(`/lyrics/${page}/${id}`);
+              navigate(`/my-tunes/lyrics/${page}/${id}`);
             }}
           >
             <FaYoutube color="deepskyblue" size={26} />
